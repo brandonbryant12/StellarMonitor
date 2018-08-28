d374a496694f04bd6edd1e030f7662f42d060afa
brandonbryant12 <brandon.bryant002@gmail.com>
2018-08-28 15:48:46 -0400

    reading and writing to an external volume

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 	"time"
 )
 
+var (
+	outfile, _ = os.Create("/data/ethLogs.log") // update path for your needs
+	l          = log.New(outfile, "", 0)
+)
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
@@ -35,8 +40,8 @@ func check(e error) {
 }
 func readLastLedger() string {
 
-	if Exists("lastLedger.txt") {
-		dat, err := ioutil.ReadFile("lastLedger.txt")
+	if Exists("/data/lastLedger.txt") {
+		dat, err := ioutil.ReadFile("/data/lastLedger.txt")
 		if err != nil {
 			// handle error
 		}
@@ -48,7 +53,7 @@ func readLastLedger() string {
 func writeLastLedgerNumber(s string) {
 	fmt.Println("write", s)
 	d1 := []byte(s)
-	err := ioutil.WriteFile("lastLedger.txt", d1, 0644)
+	err := ioutil.WriteFile("/data/lastLedger.txt", d1, 0644)
 	check(err)
 }
 
@@ -57,12 +62,12 @@ func main() {
 	conn, err := amqp.Dial(os.Getenv("AMQPConn"))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
-	log.Printf("Opened amqp connection")
+	l.Printf("Opened amqp connection")
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
-	log.Printf("opened channel")
+	l.Printf("opened channel")
 
 	err = ch.ExchangeDeclare(
 		"stellar", // name
@@ -74,7 +79,7 @@ func main() {
 		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
-	log.Printf("Exchanged declared\nname:stellar\ntype:direct\ndurable:true\nauto-deleted:false\ninternal:false\nno-wait:false\nargs:nil")
+	l.Printf("Exchanged declared\nname:stellar\ntype:direct\ndurable:true\nauto-deleted:false\ninternal:false\nno-wait:false\nargs:nil")
 
 	for {
 		lastLedgerNumber := readLastLedger()
@@ -95,10 +100,11 @@ func main() {
 		defer resp.Body.Close()
 		content, _ := ioutil.ReadAll(resp.Body)
 		if strings.Contains(string(content), "Resource Missing") {
-			log.Printf(fmt.Sprintf("Ledger not found number: %v", strconv.FormatInt(nextLedgerNumber, 10)))
+			l.Printf(fmt.Sprintf("Ledger not found number: %v", strconv.FormatInt(nextLedgerNumber, 10)))
 			time.Sleep(2000 * time.Millisecond)
 			continue
 		}
+		fmt.Println((strconv.FormatInt(nextLedgerNumber, 10)))
 		writeLastLedgerNumber(strconv.FormatInt(nextLedgerNumber, 10))
 		var ledger Ledger
 		json.Unmarshal(content, &ledger)
@@ -131,7 +137,7 @@ func main() {
 					ContentType:  "text/plain",
 					Body:         []byte(paymentJSON),
 				})
-			log.Printf(" [x] Sent %s", payment.String())
+			l.Printf(" [x] Sent %s", payment.String())
 			failOnError(err, "Failed to publish a message")
 
 		}
